441e4ec97b6cbc23a8489208b8b19640cfd512e7
mydoor3520 <mydoor3520@naver.com>
2022-04-25 20:05:55 +0900

    불필요 코드 제거

--- src/main/java/com/coinstation/coinapi/CoinApiConstants.java
+++ src/main/java/com/coinstation/coinapi/CoinApiConstants.java
@@ -8,11 +8,6 @@ public class CoinApiConstants {
 
 	public static final String DB_SOURCE_COIN_STATION = "jdbcCoinStation";
 
-	/* 코인 마켓캡 요청 url */
-	public static final String API_COIN_MARKET_CAP_LIST = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5000";
-	public static final String API_COIN_MARKET_CAP_HEADER_API_KEY = "X-CMC_PRO_API_KEY";
-	public static final String API_COIN_MARKET_CAP_HEADER_API_KEY_VALUE = "ba24a4e2-37f8-4c5a-801a-645117e63463";
-
 	/* 업비트 코인 가격 요청 url */
 	public static final String API_UPBIT_COIN_PRICE = "https://api.upbit.com/v1/ticker?markets=KRW-";
 	/* 업비트 거래소 이미지 */
